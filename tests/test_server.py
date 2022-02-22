94d1912c963a1f4595c1b25e00f137344da1b04f
Vladislav Mikheenko <mkhn.email@gmail.com>
2022-02-21 20:01:58 -0800

    AIOServer's instance async start_server method has been tested.

--- tests/test_server.py
+++ tests/test_server.py
@@ -9,3 +9,12 @@ def test_aioserver_can_be_created():
 def test_aioserver_can_be_created_with_custom_arguments():
     assert AIOServer(host=TEST_HOST, port=TEST_PORT), \
         'AIOServer instance cannot be created with custom arguments.'
+
+
+async def test_aioserver_can_be_run():
+    assert (
+        await AIOServer(
+            host=TEST_HOST,
+            port=TEST_PORT
+        ).start_server() is None
+    ), 'AIOServer\'s instance async method `start_server` does not work.'
